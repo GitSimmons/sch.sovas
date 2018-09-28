0f724a6e2d63df5b53de9d577c70f1728eba8092
GitSimmons <ben-simmons@mail.com>
2018-09-27 20:57:24 -0400

    Move h2 && h3 styles to themes as they'll be re-used

--- src/components/Mechanics.js
+++ src/components/Mechanics.js
@@ -2,20 +2,24 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledMechanics = styled.div`
-display: inline-block;
+display: block;
 margin: auto;
-width: 1500px;
+max-width: 1200px;
 text-align: left;
 position: relative;
+padding: .5rem;
 
-
+/* before and after make the corner lines */
+&::before, &::after {
+  content: '';
+  background: #99a2a8; 
+  position: absolute;
+  top:  0px;
+}
 &::after {
   content:'';
   width:160px; 
   height:1px; 
-  background: #99a2a8; 
-  position: absolute;
-  top:  0px;
   left: -1rem;
   ;
 }
@@ -24,35 +28,14 @@ position: relative;
   content:'';
   height:160px; 
   width:1px; 
-  background:#99a2a8; 
-  position:absolute;
   left: -1rem;
   ;
 }
 
-h2 {
-  font-size: 2rem;
-  font-weight: 300;
-  letter-spacing: .5rem;
-  text-transform: uppercase;
-}
-h3 {
-  font-size: 1.75rem;
-  font-weight: 300;
-  letter-spacing: .5rem;
-  position: relative;
-  text-transform: uppercase;
-  vertical-align: middle;
-}
+
 img {
   position: absolute;
   margin-left: 30px;
-}
-
-p, li {
-  font-size: .9rem;
-  font-weight: 400;
-  position: relative;
 }
 `
 
--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -6,12 +6,26 @@ export const GlobalStyle = createGlobalStyle`
 body { 
     margin: 0;
     padding: 0;
-    background-color: #002432;
+    background-image: linear-gradient(#002534, black);
     font-family: 'Raleway', sans-serif;
   }
-  p {
-    
+  p, li {
+    font-size: 0.9em;
   }
+  h2 {
+  font-size: 2rem;
+  font-weight: 300;
+  letter-spacing: .5rem;
+  text-transform: uppercase;
+}
+h3 {
+  font-size: 1.75rem;
+  font-weight: 300;
+  letter-spacing: .5rem;
+  position: relative;
+  text-transform: uppercase;
+  vertical-align: middle;
+}
   ::selection {
     background: palevioletred; /* WebKit/Blink Browsers */
   }
