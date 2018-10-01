a3590a4dc0a0e26b9e50f657f30eac9eba06b8ea
GitSimmons <ben-simmons@mail.com>
2018-10-01 10:33:04 -0400

    Add size prop to Actions component

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { Action } from './Action'
 import { getSkills } from './../skills'
 
-export const Actions = ({ actions }) => (
+export const Actions = ({ actions, size }) => (
   getSkills(actions).map(({ Icon }) =>
     <Action
       icon={Icon}
+      size={size | '48'}
     />
   )
 )
